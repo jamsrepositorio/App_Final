54facd6cf3ced5b412ce90bc642a588c181f8c4a
jair <jair.millansanchez@gmail.com>
2021-09-29 21:11:42 -0500

    mezcla productos

--- app/src/main/java/com/example/app_final/login/LoginFragment.kt
+++ app/src/main/java/com/example/app_final/login/LoginFragment.kt
@@ -1,27 +1,67 @@
 package com.example.app_final.login
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.app_final.R
 import com.example.app_final.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding: FragmentLoginBinding get() = _binding!!
+
+
+    private lateinit var auth:FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentLoginBinding.inflate(layoutInflater)
+
+
+        auth = FirebaseAuth.getInstance()
+
         return binding.root
     }
 
+
+    private fun loginUser(){
+        val user:String= binding.emailText.text.toString()
+        val contra:String=binding.editTextPassword.text.toString()
+
+
+        if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(contra)){
+            binding.progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(user,contra)
+                .addOnCompleteListener(requireActivity()){
+                    task ->
+                    if(task.isSuccessful){
+                        action()
+                    }else{
+                        Toast.makeText(requireContext(),"Error en la autenticacion", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+
+    private fun action(){
+
+    }
+
+    fun login(view:View){
+        loginUser()
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpComponent()
--- app/src/main/java/com/example/app_final/signup/SignupFragment.kt
+++ app/src/main/java/com/example/app_final/signup/SignupFragment.kt
@@ -1,25 +1,94 @@
 package com.example.app_final.signup
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.app_final.R
 import com.example.app_final.databinding.FragmentSignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import java.lang.ref.PhantomReference
 
 class SignupFragment : Fragment() {
     private var _binding: FragmentSignupBinding? = null
     private val binding: FragmentSignupBinding get() = _binding!!
+
+    private lateinit var emailText: EditText
+    private lateinit var passwordText: EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var dbReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignupBinding.inflate(layoutInflater)
+
+        database = FirebaseDatabase.getInstance()
+        auth = FirebaseAuth.getInstance()
+        dbReference = database.reference.child("User")
+
         return binding.root
     }
+
+    fun register(view: View) {
+        createNewAccount()
+    }
+
+    private fun createNewAccount() {
+        val name: String = binding.emailText.text.toString()
+        val contra: String = binding.passwordText.text.toString()
+
+
+
+
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(contra)) {
+            progressBar.visibility = View.VISIBLE
+
+            auth.createUserWithEmailAndPassword(name, contra)
+                .addOnCompleteListener(requireActivity()) { task ->
+                    if (task.isComplete) {
+                        val user: FirebaseUser? = auth.currentUser
+                        verifyEmail(user)
+
+                        val userBD = dbReference.child(user?.uid?:"")
+
+                        userBD.child("Name").setValue(name)
+                        userBD.child("Contraseña").setValue(contra)
+                    }
+                }
+
+        }
+    }
+
+    private fun verifyEmail(user: FirebaseUser?) {
+        user?.sendEmailVerification()
+            ?.addOnCompleteListener(requireActivity()) { task ->
+                if (task.isComplete) {
+                    Toast.makeText(requireContext(), "Email enviado", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(requireContext(), "Error al enviar", Toast.LENGTH_LONG).show()
+                }
+            }
+
+    }
+
+    /*private fun action() {
+        startActivity(Intent(this.LoginFragment::class.java))
+    }*/
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
